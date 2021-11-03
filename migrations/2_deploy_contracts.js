d2c03391fd9382b02b99287363cd3ad01faa52e5
Julian Kanjere <julian.kanjere@jskdezign.com>
2021-11-03 15:02:29 +0200

    run eslint --fix

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,6 +1,6 @@
-//var product = artifacts.require("TheProduct");
-var product = artifacts.require("TheProductV2");
+// var product = artifacts.require("TheProduct");
+const product = artifacts.require('TheProductV2');
 
-module.exports = function(deployer) {
-    deployer.deploy(product);
+module.exports = function (deployer) {
+  deployer.deploy(product);
 };
